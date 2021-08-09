d33ae85f751de51707da937c0e4e319bdab58af1
Fred George <fredgeorge@acm.org>
2021-08-09 17:30:49 +0200

    Fred: Removed Android and Kotlin lint sections

--- custom-ktlint-rules/src/main/kotlin/com/vanniktech/ktlintcustomrules/CustomRuleSetProvider.kt
+++
@@ -1,8 +0,0 @@
-package com.vanniktech.ktlintcustomrules
-
-import com.github.shyiko.ktlint.core.RuleSet
-import com.github.shyiko.ktlint.core.RuleSetProvider
-
-class CustomRuleSetProvider : RuleSetProvider {
-  override fun get() = RuleSet("custom-ktlint-rules", NoInternalImportRule())
-}
